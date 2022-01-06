f95b473c6e06553606a48eb1f3de4db4924449fa
Matheus Pereira <matheusdimmy@hotmail.com>
2022-01-06 16:45:35 -0300

    Requisito 1 OK

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,6 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
-}
+const getSpeciesByIds = (id1, id2) =>
+  data.species.filter(species => species.id === id1 || species.id === id2);
 
 module.exports = getSpeciesByIds;
