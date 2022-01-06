c3188bc2adc832a737c69414d4a8edc0611cebe4
Matheus Pereira <matheusdimmy@hotmail.com>
2022-01-06 17:12:01 -0300

    Corrigindo Erro Eslint Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,6 +1,6 @@
 const data = require('../data/zoo_data');
 
 const getSpeciesByIds = (id1, id2) =>
-  data.species.filter(species => species.id === id1 || species.id === id2);
+  data.species.filter((species) => species.id === id1 || species.id === id2);
 
 module.exports = getSpeciesByIds;
