04bf4007b7b20f233557cefee40ff38bcf60f1ce
Matheus Pereira <matheusdimmy@hotmail.com>
2022-01-14 00:39:43 -0300

    Requisito 6 OK

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,37 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
+
 function countEntrants(entrants) {
-  // seu código aqui.
+  const count = { child: 0, adult: 0, senior: 0 };
+
+  entrants.forEach((entrant) => {
+    if (entrant.age < 18) {
+      count.child += 1;
+    } else if (entrant.age >= 18 && entrant.age < 50) {
+      count.adult += 1;
+    } else {
+      count.senior += 1;
+    }
+  });
+
+  return count;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  // Ref. https://www.horadecodar.com.br/2020/10/06/como-verificar-se-objeto-esta-vazio-em-javascript/
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+
+  const visitors = countEntrants(entrants);
+
+  const childrens = visitors.child * prices.child;
+  const adults = visitors.adult * prices.adult;
+  const seniors = visitors.senior * prices.senior;
+
+  return childrens + adults + seniors;
 }
 
+// Ref. https://github.com/tryber/sd-018-a-project-zoo-functions/pull/11
 module.exports = { calculateEntry, countEntrants };
